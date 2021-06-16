553d4e121b78ec7cfc7c7ba6e0000aea9cd377ca
nalejivan <nalejivan@gmail.com>
2021-06-16 16:26:40 +0300

    16.06.2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,14 +4,13 @@ import { connect } from 'react-redux';
 import { loginThunk, logOutThunk } from '../../redux/reducers/auth_reducer';
 import { Redirect } from 'react-router-dom';
 
-const Login = (props) => {
-  const handleSubmit = ({ email, password, rememberMe = true, captcha = true }) => 
-        props.loginThunk(email, password, rememberMe, captcha);
-  if(props.auth === true){
-    return <Redirect to={"profile/" + props.userId}/>
+const Login = ({auth, userId, loginThunk}) => {
+  if(auth === true){
+    return <Redirect to={"profile/" + userId}/>
   }
-  
-  return <LoginFrom onSubmit={handleSubmit}/>
+  const handleSubmit = ({ email, password, rememberMe = true, captcha = true }) => 
+        loginThunk(email, password, rememberMe, captcha)
+  return <LoginFrom onSubmit={handleSubmit} email={userId}/>
 
 
 }
